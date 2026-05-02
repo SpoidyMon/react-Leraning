04ed7ee4262626c31b7a60d4ee47c91b2127f18b
Mrunal Jagtap <mrunaljagtap395@gmail.com>
2026-05-02 14:17:11 +0530

    06-tailwind-css

---
+++ 06-tailwind_css/src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      This is Tailwind part
+    </div>
+  )
+}
+
+export default App
